eac6b0b508d5ef949c7b46ad762023002a6c49e5
mj0se <mariajose1907@gmail.com>
2020-04-21 19:15:34 +0200

    add docs

---
+++ docs/precache-manifest.ca4f1ecd3f71e283cd4a2543b6d9d14a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "119f0584856b4c61c3d9de47e0a62b13",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e42ec9d16a4a67ab2c96",
+    "url": "./static/css/main.5c48e7e3.chunk.css"
+  },
+  {
+    "revision": "317213a47f44b836c7b5",
+    "url": "./static/js/2.6cf0ceac.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.6cf0ceac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e42ec9d16a4a67ab2c96",
+    "url": "./static/js/main.4df038a0.chunk.js"
+  },
+  {
+    "revision": "f9e32c010b5fef188e8a",
+    "url": "./static/js/runtime-main.1f51c63f.js"
+  },
+  {
+    "revision": "c548701c88cb0dd8dc5e5681c4ace619",
+    "url": "./static/media/header.c548701c.jpg"
+  },
+  {
+    "revision": "dd03b04a3b688da4980cea9f3af3430d",
+    "url": "./static/media/logo.dd03b04a.png"
+  }
+]);
